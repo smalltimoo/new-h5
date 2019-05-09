59238302c5380d6541b6afa18ff3c8bec21eed0d
steven <648800939@qq.com>
2019-05-09 15:28:22 +0700

    chongzhi

--- src/views/Member/OnlineDeposit.vue
+++ src/views/Member/OnlineDeposit.vue
@@ -90,10 +90,8 @@
                              @click="mUpdateMoneys(it)"
                         >
                             <Radio :label="it.id">
-                                <img src="../../assets/images/alipay.png" width="20px"
-                                     style="margin-left: 8px;margin-right: 8px"
-                                />
-                                <span> {{$t('member.onlineDeposit.od9')}} </span>&nbsp;&nbsp;&nbsp;
+                                <img src="../../assets/images/alipay.png" width="20px" style="margin-left: 8px;margin-right: 8px"/>
+                                <span> {{$t('member.onlineDeposit.od9')}} <span v-if="it.typeid==5">APP</span></span>&nbsp;&nbsp;&nbsp;
                                 <span> {{$t('member.onlineDeposit.od13')}}:{{ it.minMoney}} - {{it.maxMoney}} {{$t('member.onlineDeposit.od14')}} </span>
                                 <span class="radio-id">{{ it.apiid}}</span>
                             </Radio>
@@ -104,9 +102,8 @@
                              @click="mUpdateMoneys(it)"
                         >
                             <Radio :label="it.id">
-                                <img src="../../assets/images/wechat.png" width="20px"
-                                     style="margin-left: 8px;margin-right: 8px"/>
-                                <span> {{$t('member.onlineDeposit.od10')}} </span>
+                                <img src="../../assets/images/wechat.png" width="20px" style="margin-left: 8px;margin-right: 8px"/>
+                                <span> {{$t('member.onlineDeposit.od10')}} <span v-if="it.typeid==4">app</span></span>&nbsp;&nbsp;&nbsp;
                                 <span> {{$t('member.onlineDeposit.od13')}}:&nbsp; {{ it.minMoney}} - {{it.maxMoney}} {{$t('member.onlineDeposit.od14')}} </span>
                                 <span class="radio-id">{{ it.apiid}}</span>
                             </Radio>
