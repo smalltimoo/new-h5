71f7beaa56d4ea2f5a4bb74d092198d454d13f7e
steven <648800939@qq.com>
2019-05-21 09:37:12 +0700

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -116,7 +116,7 @@
                         <li class="icon-kefu" @click="mOpenCService">
                             {{$t('customservice')}}
                         </li>
-                        <li class="icon-qq" v-if="agentQQ">
+                        <li class="icon-qq" v-if="cQQ1 || cQQ2">
                             <a v-if="cQQ1" :href="'mqqwpa://im/chat?chat_type=wpa&uin='+cQQ1+'&version=1&src_type=web'">
                                 {{cQQ1}}
                             </a>
